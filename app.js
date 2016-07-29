2819ff806b4ca98bac113b5c92f862c8e22a1eb0
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2016-07-29 14:07:11 -0500

    se cambia configuracion de login para que funcione con mlab para mongo

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 //mongoose.connect('mongodb://localhost:27017/ejemplo1');
 
-mongoose.connect('mongodb://ds031865.mlab.com:31865/ejemplo1');
+mongoose.connect('mongodb://oxrl:Majcp071102@ds031865.mlab.com:31865/ejemplo1');
 
 //mongodb://<dbuser>:<dbpassword>@ds031865.mlab.com:31865/ejemplo1
 app.use('/', routes);
