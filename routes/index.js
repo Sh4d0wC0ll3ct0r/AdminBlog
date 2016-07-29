1d79382b1ec5b79701bb855738c9728fc382dee9
Xavier Romero Lopez <o.romero.lopez@gmail.com>
2016-07-28 22:22:09 -0500

    Se actulizo el las url de login

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  /*res.render('index', { title: 'Express' });*/
+    res.redirect('/admin/login');
 });
-
+router.get('/admin', function(req, res, next) {
+    /*res.render('index', { title: 'Express' });*/
+    res.redirect('/admin/login');
+});
 
 
 router.get('/posts',function(req,res,next){
